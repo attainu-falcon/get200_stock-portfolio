3a4449e75b51e28f27c26ffdad2f7d27aee2e135
surajk202 <kawalesuraj76@gmail.com>
2019-06-23 00:32:14 +0530

    login/signup

--- portfoliopage.js
+++ portfoliopage.js
@@ -8,12 +8,12 @@ var ObjectId = require('mongodb').ObjectID;
 router.use(express.static('public'));
 
 router.get('/',function(req,res){
-       res.sendfile('public/portfolio.html');
+      
 });
 
 
 router.get('/addportfolio',function(req,res){
-    res.send('add portfolio');
+    
 });
 
 
